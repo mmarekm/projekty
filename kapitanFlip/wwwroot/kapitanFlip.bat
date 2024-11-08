5e627c69ad28f70888751fcaf9dec6d2aca1d937
Marek <marekwpracymalicki@gmail.com>
2024-11-08 11:50:35 +0100

    dzialaBat

--- kapitanFlip/wwwroot/kapitanFlip.bat
+++ kapitanFlip/wwwroot/kapitanFlip.bat
@@ -1,15 +1,17 @@
 @echo off
-echo Running npm install...
-cd /d "C:\Users\marek\Desktop\projekty gitHub\kapitanFlip\wwwroot"
-"C:\Program Files\nodejs\npm.cmd" install
+cd /d C:\Users\marek\Desktop\projekty gitHub\kapitanFlip\wwwroot
 
-echo Starting http-server...
-start "" "C:\Users\marek\AppData\Roaming\npm\node_modules\http-server\bin\http-server" . -p 8080
+REM Sprawdz czy http-server jest juz zainstalowany
+where http-server >nul 2>nul
+if %errorlevel% neq 0 (
+    echo Instalowanie http-server...
+    npm install -g http-server
+)
 
-echo Waiting for server to start...
-timeout /t 10
+echo Uruchamianie serwera i przegladarki...
 
-echo Opening Firefox...
-start "" "C:\Program Files\Mozilla Firefox\firefox.exe" http://127.0.0.1:8080
+REM Uruchom Firefox w nowym oknie po 2 sekundach opoznienia
+start "" cmd /c "timeout /t 2 /nobreak && start firefox http://127.0.0.1:8080"
 
-pause
+REM Uruchom serwer
+cmd /k "http-server"
